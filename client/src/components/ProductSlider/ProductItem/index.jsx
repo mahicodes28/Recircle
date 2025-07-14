fde5622ee3bc68d51c8c798e961c7fb68247be27
Abhimanyu Saxena <abhimanyusaxena1>
2025-07-14 16:47:10 +0530

    removed python

--- client/src/components/ProductSlider/ProductItem/index.jsx
+++ client/src/components/ProductSlider/ProductItem/index.jsx
@@ -20,11 +20,11 @@ const ProductItem = ({product}) => {
   return (
     <>
       <div className="productItem scale-90 bg-[#27272A] transition-all hover:text-black hover:bg-white hover:scale-95 overflow-hidden !rounded-sm xl:rounded-sm w-full xl:min-h-[28vh] xl:min-h-[22vw] md:min-h-[22vw]" >
-        <div className="imgWrapper relative w-full !h-[18vh]  md:!h-[10vw] bg-red-200 xl:!h-[12vw]">
+        <div className="imgWrapper relative w-full h-[18vh]  md:!h-[10vw]   xl:!h-[12vw]">
           {/* {`/product/${product._id}`} link per ye path dalega  */}
           <Link onClick={() => setProductId(product._id)} to="/productDetails" className='link w-full h-[18vh] xl:h-[13vw] block'>
             {product?.image?.length > 0 && (
-  <img src={product.image[0]}  />
+  <img src={product.image[0]} className='w-full h-full overflow-hidden object-cover' />
 )}
           </Link>
           <div className="Actions hidden md:flex opacity-0 hover:opacity-100 gap-2 absolute top-2 right-2 md:top-[1vw] md:right-[1vw] w-10 md:w-[2vw] flex-col items-center justify-between px-2">
@@ -54,14 +54,14 @@ const ProductItem = ({product}) => {
           </div>
         </div>
         <div className="!info !px-2 !pt-2 !pb-2 xl:!px-4 xl:!pt-4 min-h-[10vh]   xl:min-h-[12vw]  md:min-h-[12vw]  text-left w-full flex flex-col justify-center items-start">
-          <h6 className='Brand w-full text-left text-xs md:!text-md xl:!text-md  md:text-base transition-all capitalize'><Link className='link' to="/">{product.product_name}</Link></h6>
-          <h3 className='Title w-full text-left text-sm xl:!text-xl transition-all'><Link className='link capitalize' to="/">{product.category}</Link></h3>
+          <h6 className='Brand w-full text-left text-xs md:!text-md xl:!text-md  md:text-base transition-all capitalize'><Link className='link' to="/">{product?.product_name || 'No Name'}</Link></h6>
+          <h3 className='Title w-full text-left text-sm xl:!text-xl transition-all'><Link className='link capitalize' to="/">{product?.category || 'No Category'}</Link></h3>
           <Stack spacing={1} className=''>
             <Rating name="half-rating-read" className='!scale-40 xl:!scale-80 absolute  left-1' defaultValue={2.5} precision={0.5} readOnly />
           </Stack>
           <div className="flex items-center gap-2 mt-1">
-            <span className='oldPrice text-xs xl:!text-lg text-zinc-400 line-through'>${product.price}</span>
-            <span className='newPrice text-xs xl:!text-lg font-semibold ml-2 text-red-500'>${product.offerPrice}</span>
+            <span className='oldPrice text-xs xl:!text-lg text-zinc-400 line-through'>${product?.price ?? '--'}</span>
+            <span className='newPrice text-xs xl:!text-lg font-semibold ml-2 text-red-500'>${product?.offerPrice ?? '--'}</span>
           </div>
         </div>
       </div>
